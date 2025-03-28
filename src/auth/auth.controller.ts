e6ad87a61935eb0ee9ebd2775f6a8b78042c4079
Dong Hyun <decidere@naver.com>
2025-03-29 06:16:33 +0900

    [Feat] 회원가입 로직 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  // 회원 가입
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    return await this.authService.register(registerDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 회원 가입
+  async register(registerDto: RegisterDto) {
+    const existEmail = await this.prisma.users.findUnique({
+      where: { email: registerDto.email },
+    });
+
+    if (existEmail) throw new ConflictException('이미 존재하는 이메일 입니다.');
+
+    await this.prisma.users.create({
+      data: {
+        email: registerDto.email,
+        password: registerDto.password,
+        name: registerDto.name,
+      },
+    });
+
+    return { success: true, message: '회원가입이 완료되었습니다.' };
+  }
+}
