6d62d94774cca5a06778021217b7741a383cd84a
Dong Hyun <decidere@naver.com>
2025-03-29 18:15:21 +0900

    [Feat] 로그인 로직 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,15 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
 
   // 회원 가입
   async register(registerDto: RegisterDto) {
@@ -27,5 +32,30 @@ export class AuthService {
     });
 
     return { success: true, message: '회원가입이 완료되었습니다.' };
+  }
+
+  // 로그인
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.users.findUnique({
+      where: { email: loginDto.email },
+    });
+
+    if (!user) throw new Error('계정이 존재하지 않습니다.');
+
+    const validPassword = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (validPassword) {
+      const accessToken = this.jwt.sign({
+        sub: user.id,
+        email: user.email,
+        name: user.name,
+      });
+      return { success: true, message: '로그인에 성공했습니다', accessToken };
+    } else {
+      throw new Error('비밀번호가 일치하지 않습니다.');
+    }
   }
 }
