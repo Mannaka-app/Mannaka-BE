855d070d2c870518e5ea4f814e5c6deb2d4b163e
Dong Hyun <decidere@naver.com>
2025-03-31 21:29:12 +0900

    [Fix] 오류 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -87,7 +87,7 @@ export class AuthService {
         new URLSearchParams({
           grant_type: 'authorization_code',
           client_id: process.env.KAKAO_REST_API_KEY,
-          redirect_uri: 'http://localhost:3000/auth/kakao/callback',
+          redirect_uri: 'https://yeol.store/auth/kakao/callback',
           code,
         }),
         {
